e4bbeb22e581989a6c379f21857038d4b622edce
kaijhsu <hikper@gmail.com>
2022-10-05 12:23:21 +0800

    pass clamp float

--- hw1/part1/vectorOP.cpp
+++ hw1/part1/vectorOP.cpp
@@ -40,6 +40,33 @@ void absVector(float *values, float *output, int N)
   }
 }
 
+// decrease exponents by 1 until 0 and return multmask has any 1 or not;
+bool convert_mult_mask(__pp_vec_int &exponents, __pp_mask &dest_mult_mask){
+  __pp_mask allpass = _pp_init_ones();
+  
+  __pp_vec_int vec_zero;
+  _pp_vset_int(vec_zero, 0, allpass);
+
+  __pp_mask vec_vgt_zero;
+  _pp_vgt_int(vec_vgt_zero, exponents, vec_zero, allpass);
+
+  __pp_vec_int vec_one;
+  _pp_vset_int(vec_one, 1, allpass);
+  _pp_vsub_int(exponents, exponents, vec_one, vec_vgt_zero);
+  
+  dest_mult_mask = vec_vgt_zero;
+  return (_pp_cntbits(vec_vgt_zero) > 0)? 1: 0;
+}
+
+void clamp_float(__pp_vec_float &vec_source, float limit = 9.999999){
+  __pp_mask allpass = _pp_init_ones();
+  __pp_vec_float vec_limit = _pp_vset_float(limit);
+  __pp_mask limit_mask = _pp_init_ones();
+
+  _pp_vgt_float(limit_mask, vec_source, vec_limit, allpass);
+  _pp_vmove_float(vec_source, vec_limit, limit_mask);
+}
+
 void clampedExpVector(float *values, int *exponents, float *output, int N)
 {
   //
@@ -49,8 +76,30 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
   // Your solution should work for any value of
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
   //
+  __pp_mask allpass = _pp_init_ones();
+
   for (int i = 0; i < N; i += VECTOR_WIDTH)
   {
+
+    __pp_vec_float vec_value;
+    __pp_vec_int vec_exponents;
+
+    _pp_vload_float(vec_value, &values[i], allpass);
+    _pp_vload_int(vec_exponents, &exponents[i], allpass);
+
+    __pp_vec_float vec_result = _pp_vset_float(1.0);
+
+    __pp_mask multMask;
+    while(convert_mult_mask(vec_exponents, multMask)){
+      _pp_vmult_float(vec_result, vec_result, vec_value, multMask);
+      clamp_float(vec_result);
+      // printf("test\n");
+      // for (int i = 0; i < N; i++)
+      //   printf("% f ", output[i]);
+      // printf("\n");
+    }
+    _pp_vstore_float(&output[i], vec_result, allpass);
+
   }
 }
 
